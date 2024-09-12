f31f28e271373cbd1e1f13cd0e05f9ceece12439
jplaikows <joshua@laikowski.com>
2024-09-12 01:13:48 -0400

    Added Prime React

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -2,15 +2,19 @@ import { NextRequest, NextResponse } from "next/server";
 import db from "@/lib/primsa";
 
 export async function GET(req: any) {
+  // const { phone }: { phone: string } = await req.json();
+
+  const user = await db.user.create({ data: { phone: "00000" } });
   return NextResponse.json({ message: "Hello World" }, { status: 200 });
 }
 
 export async function POST(req: NextRequest) {
   const { phone }: { phone: string } = await req.json();
 
-  const authenticationNumber = Math.floor(100000 + Math.random() * 900000);
+  const user = await db.user.findMany();
+  // const authenticationNumber = Math.floor(100000 + Math.random() * 900000);
 
-  console.log(authenticationNumber, " ", phone);
+  // console.log(authenticationNumber, " ", phone);
 
-  return NextResponse.json({ message: "success" }, { status: 200 });
+  return NextResponse.json({ user }, { status: 200 });
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { PrimeReactProvider } from "primereact/api";
 import "./globals.css";
 
 const geistSans = localFont({
@@ -28,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <PrimeReactProvider>{children}</PrimeReactProvider>
       </body>
     </html>
   );
