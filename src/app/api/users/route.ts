8513cf89e57b013c6bd3b5a808b64b77c8778901
jplaikows <joshua@laikowski.com>
2024-09-12 01:58:28 -0400

    Added Email Column

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -11,7 +11,12 @@ export async function GET(req: any) {
 export async function POST(req: NextRequest) {
   const { phone }: { phone: string } = await req.json();
 
-  const user = await db.user.findMany();
+  const user = await db.user.findFirst({ where: { phone } });
+
+  if (!user) {
+    const newUser = db.user.create({ data: { phone } });
+    return NextResponse.json({ newUser }, { status: 200 });
+  }
   // const authenticationNumber = Math.floor(100000 + Math.random() * 900000);
 
   // console.log(authenticationNumber, " ", phone);
