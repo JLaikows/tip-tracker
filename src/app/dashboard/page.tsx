6c88855fc9626bef7ea11d806aa2164f07084646
jplaikows <joshua@laikowski.com>
2024-09-14 02:49:27 -0400

    Broke payout create form into own component and updated styling

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,15 +1,11 @@
 "use client";
 
+import PayoutCreateForm from "@/lib/components/PayoutCreateForm";
 import { payout } from "@prisma/client";
 import axios from "axios";
-import { Button } from "primereact/button";
-import { Checkbox } from "primereact/checkbox";
 import { Dropdown } from "primereact/dropdown";
-import { InputNumber, InputNumberChangeEvent } from "primereact/inputnumber";
-import { InputText } from "primereact/inputtext";
 import { Panel } from "primereact/panel";
-import { FormEventHandler, useCallback, useEffect, useState } from "react";
-import { toast } from "react-toastify";
+import { useCallback, useEffect, useState } from "react";
 
 const now = new Date(Date.now());
 
@@ -20,44 +16,18 @@ const defaultPayout = {
   userId: 0,
   date: now.toISOString().slice(0, 10),
   client: "",
+  owed: 0,
 };
 
 export default function Home() {
-  const [formData, setFormData] = useState(defaultPayout);
-  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
   const [payouts, setPayouts] = useState<[]>([]);
   const [selectedId, setSelectedId] = useState({ name: "1", code: 1 });
-
-  const handleChange = (name: string) => (e: InputNumberChangeEvent) => {
-    setFormData({ ...formData, [name]: e.value });
-  };
 
   const getPayouts = useCallback(async () => {
     const { data } = await axios.get(`/api/users/${selectedId.code}/payouts`);
 
     setPayouts(data.payouts);
   }, [selectedId]);
-
-  const handleOriginalChange = (e: {
-    target: { value: number | string; name: string };
-  }) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
-    e.preventDefault();
-    setIsSubmitting(true);
-
-    const { data } = await axios.post("/api/payouts", { ...formData });
-    if (data.error) {
-      toast.error(data.error);
-    } else {
-      toast.success("Successfull Payout!");
-      getPayouts();
-    }
-
-    setIsSubmitting(false);
-  };
 
   useEffect(() => {
     getPayouts();
@@ -70,69 +40,9 @@ export default function Home() {
   ];
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Panel header="Add Payout" className="b-2">
-          <form
-            className="flex flex-col gap-4 justify-items-center items-center"
-            onSubmit={handleSubmit}
-          >
-            <label>Amount</label>
-            <InputNumber
-              name="amount"
-              value={formData.amount}
-              onChange={handleChange("amount")}
-              minFractionDigits={0}
-              maxFractionDigits={2}
-            />
-            <input
-              type="date"
-              name="date"
-              onChange={handleOriginalChange}
-              value={formData.date}
-            />
-            <div>
-              <Checkbox
-                checked={formData.taxable}
-                value={formData.taxable}
-                onChange={() =>
-                  setFormData({ ...formData, taxable: !formData.taxable })
-                }
-                name="taxable"
-              />
-              <label>Taxable</label>
-            </div>
-            <InputText
-              size="large"
-              name="state"
-              placeholder="State"
-              onChange={handleOriginalChange}
-              value={formData.state}
-            />
-            <InputText
-              size="large"
-              placeholder="Client"
-              name="client"
-              onChange={handleOriginalChange}
-              value={formData.client}
-            />
-            <label>UserId</label>
-            <InputNumber
-              value={formData.amount}
-              onChange={handleChange("userId")}
-              maxFractionDigits={0}
-            />
-            <Button
-              className="p-button p-2"
-              size="large"
-              severity="info"
-              raised
-              disabled={isSubmitting}
-            >
-              Submit
-            </Button>
-          </form>
-        </Panel>
+    <div className="flex items-center justify-items-center max-h-screen font-[family-name:var(--font-geist-sans)] p-4 ">
+      <main className="flex flex-col gap-8 row-start-2 items-center min-h-80 sm:items-start md:flex-row">
+        <PayoutCreateForm />
         <Panel header="Payouts">
           <Dropdown
             value={selectedId.code}
---
+++ src/lib/components/PayoutCreateForm.tsx
@@ -0,0 +1,202 @@
+"use client";
+
+import { payout } from "@prisma/client";
+import axios from "axios";
+import { Button } from "primereact/button";
+import { Checkbox } from "primereact/checkbox";
+import { Dropdown } from "primereact/dropdown";
+import { FloatLabel } from "primereact/floatlabel";
+import { InputNumber, InputNumberChangeEvent } from "primereact/inputnumber";
+import { InputText } from "primereact/inputtext";
+import { Panel } from "primereact/panel";
+import { FormEventHandler, useEffect, useState } from "react";
+import { toast } from "react-toastify";
+
+const now = new Date(Date.now());
+
+const states = [
+  "AL",
+  "AK",
+  "AS",
+  "AZ",
+  "AR",
+  "CA",
+  "CO",
+  "CT",
+  "DE",
+  "DC",
+  "FM",
+  "FL",
+  "GA",
+  "GU",
+  "HI",
+  "ID",
+  "IL",
+  "IN",
+  "IA",
+  "KS",
+  "KY",
+  "LA",
+  "ME",
+  "MH",
+  "MD",
+  "MA",
+  "MI",
+  "MN",
+  "MS",
+  "MO",
+  "MT",
+  "NE",
+  "NV",
+  "NH",
+  "NJ",
+  "NM",
+  "NY",
+  "NC",
+  "ND",
+  "MP",
+  "OH",
+  "OK",
+  "OR",
+  "PW",
+  "PA",
+  "PR",
+  "RI",
+  "SC",
+  "SD",
+  "TN",
+  "TX",
+  "UT",
+  "VT",
+  "VI",
+  "VA",
+  "WA",
+  "WV",
+  "WI",
+  "WY",
+];
+
+const defaultPayout = {
+  amount: 0,
+  taxable: false,
+  state: "NJ",
+  userId: 0,
+  date: now.toISOString().slice(0, 10),
+  client: "",
+  owed: 0,
+};
+
+export default function PayoutCreateForm() {
+  const [formData, setFormData] = useState(defaultPayout);
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+
+  const handleChange = (name: string) => (e: InputNumberChangeEvent) => {
+    setFormData({ ...formData, [name]: e.value });
+  };
+
+  const handleOriginalChange = (e: {
+    target: { value: number | string; name: string };
+  }) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+
+    const { data } = await axios.post("/api/payouts", { ...formData });
+    if (data.error) {
+      toast.error(data.error);
+    } else {
+      toast.success("Successfull Payout!");
+    }
+
+    setIsSubmitting(false);
+  };
+
+  return (
+    <Panel header="Add Payout" className="b-2 min-h-96">
+      <form
+        className="flex flex-col gap-4 justify-items-left p-0 gap-8"
+        onSubmit={handleSubmit}
+      >
+        <input
+          type="date"
+          name="date"
+          onChange={handleOriginalChange}
+          value={formData.date}
+        />
+        <div className="flex flex-row gap-8">
+          <FloatLabel>
+            <label>Cash</label>
+            <InputNumber
+              name="amount"
+              value={formData.amount}
+              size={9}
+              onChange={handleChange("amount")}
+              minFractionDigits={0}
+              maxFractionDigits={2}
+              tooltip="Amount of Cash taken home"
+              variant="filled"
+            />
+          </FloatLabel>
+          <FloatLabel>
+            <label>Owed</label>
+            <InputNumber
+              name="amount"
+              size={9}
+              value={formData.amount}
+              onChange={handleChange("amount")}
+              minFractionDigits={0}
+              maxFractionDigits={2}
+              variant="filled"
+            />
+          </FloatLabel>
+        </div>
+        <div className="flex flex-row gap-8">
+          <InputText
+            size={14}
+            placeholder="Client"
+            name="client"
+            onChange={handleOriginalChange}
+            value={formData.client}
+          />
+          <Dropdown
+            name="state"
+            options={states}
+            value={formData.state}
+            onChange={handleOriginalChange}
+          />
+        </div>
+        <FloatLabel>
+          <InputNumber
+            value={formData.amount}
+            onChange={handleChange("userId")}
+            maxFractionDigits={0}
+            variant="filled"
+          />
+          <label>UserId</label>
+        </FloatLabel>
+        <div>
+          <Checkbox
+            checked={formData.taxable}
+            value={formData.taxable}
+            onChange={() =>
+              setFormData({ ...formData, taxable: !formData.taxable })
+            }
+            name="taxable"
+          />
+          <label>Taxable</label>
+        </div>
+        <Button
+          className="p-button p-2 flex items-center justify-content-center"
+          severity="info"
+          raised
+          disabled={isSubmitting}
+        >
+          Submit
+        </Button>
+      </form>
+    </Panel>
+  );
+}
