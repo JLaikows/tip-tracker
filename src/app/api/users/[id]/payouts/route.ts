c5d64f49a401c86d851a051e3665a152604cc050
jplaikows <joshua@laikowski.com>
2024-09-14 15:07:37 -0400

    Updated payout route to parse payouts

--- src/app/api/users/[id]/payouts/route.ts
+++ src/app/api/users/[id]/payouts/route.ts
@@ -1,8 +1,31 @@
 import db from "@/lib/primsa";
+import { payout } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
+
+export type payoutDays = Record<number, payout[]>;
+export type payoutMonths = Record<number, payoutDays>;
 
 export async function GET(req: NextRequest) {
   const userId = Number(req.url.split("/")[5]);
   const payouts = await db.payout.findMany({ where: { userId } });
-  return NextResponse.json({ payouts }, { status: 200 });
+
+  const parsedPayouts: payoutMonths = {};
+
+  const today = new Date(Date.now());
+
+  payouts.forEach((payout) => {
+    if (today.getFullYear() != payout.year) return;
+
+    if (!parsedPayouts[payout.month]) {
+      parsedPayouts[payout.month] = {};
+    }
+
+    if (!parsedPayouts[payout.month][payout.day]) {
+      parsedPayouts[payout.month][payout.day] = [];
+    }
+
+    parsedPayouts[payout.month][payout.day].push(payout);
+  });
+
+  return NextResponse.json({ payouts: parsedPayouts }, { status: 200 });
 }
