1897748349b76ba48a6f8ee7e111eef44f83da9d
jplaikows <joshua@laikowski.com>
2024-09-28 16:35:05 -0400

    Refactored Logout

--- src/app/logout/page.tsx
+++ src/app/logout/page.tsx
@@ -5,13 +5,13 @@ import axios from "axios";
 
 export default async function Logout() {
   const token = cookies().get(COOKIES.Authorization)?.value;
+  const base = process.env.NEXT_PUBLIC_API_URL;
 
   if (!token) {
     redirect("/login", RedirectType.push);
   }
 
   try {
-    const base = process.env.NEXT_PUBLIC_API_URL;
     await axios.get(`${base}/api/users`);
   } catch (e: unknown) {
     return <>Failed to log out: {(e as { message: string }).message}</>;
@@ -19,28 +19,3 @@ export default async function Logout() {
     redirect("/login", RedirectType.push);
   }
 }
-
-// import { redirect, RedirectType } from "next/navigation";
-// import { useRouter } from "next/navigation";
-// import axios from "axios";
-
-// export default function Logout() {
-//   const base = process.env.NEXT_PUBLIC_API_URL;
-
-//   const logout = async () => {
-//     try {
-//       const { data } = await axios.get(`${base}/api/users`);
-
-//       if (data.error) throw new Error(data.error);
-
-//       console.log("ran successfully");
-
-//       redirect("/login", RedirectType.push);
-//     } catch (e: unknown) {
-//       console.log(e);
-//       return <>Failed to log out: {(e as { message: string }).message}</>;
-//     }
-//   };
-
-//   return logout();
-// }
