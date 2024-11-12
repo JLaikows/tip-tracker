f8fca4a4ac898c7928d08d676626d326eb3de662
jplaikows <joshua@laikowski.com>
2024-11-12 02:21:42 -0500

    Added payout delete route

---
+++ src/app/api/payouts/[id]/route.ts
@@ -0,0 +1,23 @@
+import db from "@/lib/primsa";
+import { COOKIES } from "@/lib/types";
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function DELETE(req: NextRequest) {
+  const token = cookies().get(COOKIES.Authorization)?.value;
+  const session = await db.session.findFirst({ where: { token } });
+
+  if (!session?.userId) {
+    cookies().delete(COOKIES.Authorization);
+    return NextResponse.json({ error: "Session Not Found" }, { status: 200 });
+  }
+
+  const id = Number(req.url.split("/")[5]);
+
+  const payout = db.payout.delete({ where: { id } });
+
+  return NextResponse.json(
+    { payout: payout, message: "successful delete" },
+    { status: 200 }
+  );
+}
